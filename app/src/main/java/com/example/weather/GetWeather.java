2b9a5c3fba862e27af2d37a228c2f09176f2d0f6
zinton <zhilitskianton05@gmail.com>
2023-05-15 14:50:48 +0200

    getting coordinates DONE

--- app/src/main/java/com/example/weather/GetWeather.java
+++ app/src/main/java/com/example/weather/GetWeather.java
@@ -1,35 +1,47 @@
 package com.example.weather;
 
 import java.io.BufferedReader;
-import java.io.ByteArrayOutputStream;
+import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
-
-import javax.net.ssl.HttpsURLConnection;
 
 public class GetWeather {
     private static final String BASE_URL = "https://api.openweathermap.org/data/2.5/weather?lat=";
     private static final String IMG_URL = "http://openweathermap.org/img/w/";
-    private String apiKey;
+    private final String apiKey;
     private double latitude;
     private double longitude;
+
     public GetWeather(String apiKey) {
         this.apiKey = apiKey;
     }
-    public void setCoords(double latitude, double longitude){
+
+    public void setCoords(double latitude, double longitude) {
         this.latitude = latitude;
         this.longitude = longitude;
     }
 
-    public String getWeatherData(String location) {
+    public String getWeatherData() {
+        URL url;
         try {
-            URL url = new URL(BASE_URL);
+            url = new URL(BASE_URL + latitude + "&lon=" + longitude + "&appid=" + apiKey);
         } catch (MalformedURLException e) {
             throw new RuntimeException(e);
         }
-        return null;
+        try (InputStream input = url.openStream()) {
+            InputStreamReader isr = new InputStreamReader(input);
+            BufferedReader reader = new BufferedReader(isr);
+            StringBuilder json = new StringBuilder();
+            int c;
+            while ((c = reader.read()) != -1) ;
+            {
+                json.append((char) c);
+            }
+            return json.toString();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
