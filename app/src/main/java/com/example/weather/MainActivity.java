80870499e46c7dbbdf3468cd2bd9456356ce93cf
zinton <zhilitskianton05@gmail.com>
2023-06-03 22:23:14 +0200

    Main functionality is done

--- app/src/main/java/com/example/weather/MainActivity.java
+++ app/src/main/java/com/example/weather/MainActivity.java
@@ -6,18 +6,15 @@ import androidx.core.content.ContextCompat;
 
 import android.Manifest;
 import android.content.Context;
+import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
 import android.os.Bundle;
-import android.os.StrictMode;
 import android.util.Log;
-import android.widget.TextView;
-
-import com.google.gson.Gson;
-
-import java.lang.reflect.Type;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity implements LocationListener {
     private static final int REQUEST_LOCATION_PERMISSION = 1;
@@ -33,12 +30,22 @@ public class MainActivity extends AppCompatActivity implements LocationListener 
         if(ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED){
             ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, REQUEST_LOCATION_PERMISSION);
         }
-        locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
-        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, this);
+        Intent myIntent = new Intent(MainActivity.this, SettingsActivity.class);
+        Button settings = (Button) findViewById(R.id.SettingsButton);
+        settings.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View view) {
+                startActivity(myIntent);
+            }
+        });
+//        locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+//        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, this);
     }
     @Override
     public void onLocationChanged(Location location){
-        GetWeather getWeather = new GetWeather("");//TODO: Add Api Key
+        final String apiKey = "";
+        GetWeather getWeather = new GetWeather(apiKey);//TODO: Add Api Key
         getWeather.setCoords(location.getLatitude(), location.getLongitude());
         Thread thread = new Thread(new Runnable(){
             @Override
